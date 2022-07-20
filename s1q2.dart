fd088359650d1ffaff64b06809f4aa0756cc110c
Gabriel Amaro <gamaro919@gmail.com>
2022-07-20 19:16:28 -0300

    initial commit

---
+++ s1q2.dart
@@ -0,0 +1,18 @@
+void main() {
+  int idade = 10;
+  String resultado;
+
+  idade <= 9
+      ? resultado = 'Fraldinha'
+      : idade == 10 || idade == 11
+          ? resultado = 'Pré-mirim'
+          : idade == 12 || idade == 13
+              ? resultado = 'Mirim'
+              : idade == 14 || idade == 15
+                  ? resultado = 'Infaltil'
+                  : idade >= 16 && idade <= 18
+                      ? resultado = 'Juvenil'
+                      : resultado = 'Idade inválida';
+
+  print(resultado);
+}
